2098225846e739c022cbd3c15dcbaafa120ff893
xlopok <44916594+xlopok@users.noreply.github.com>
2019-02-04 12:50:47 -0800

    changes

--- index.php
+++ index.php
@@ -30,23 +30,22 @@ $user_name = 'Nikita Vorobev'; // укажите здесь ваше имя
 
         <!-- здесь должен быть PHP код для показа имени пользователя -->
         <?php 
-        if($is_auth == 1) {
-           echo "<div class="user-menu__logged">
-             <p>$user_name</p>
-            </div>"
-        } else {
-            echo  "<ul class="user-menu__list">
+        if($is_auth == 1): 
+          echo '<div class="user-menu__logged">
+             <p>'.$user_name.'</p>
+            </div>'; 
+            
+        else:
+           echo '<ul class="user-menu__list">
                         <li class="user-menu__item">
-                        <a href="">Регистрация</a>
+                        <a href="#">Регистрация</a>
                         </li>
                         <li class="user-menu__item">
-                        <a href="">Вход</a>
+                        <a href="#">Вход</a>
                         </li>
-                    </ul>"
-        } 
-        
+                    </ul>';
+        endif;
         ?>
-
         </nav>
     </div>
 </header>
