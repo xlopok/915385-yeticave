b277765095a3f051adcf598653594ef8d48e4438
xlopok <44916594+xlopok@users.noreply.github.com>
2019-02-14 13:54:27 -0800

    module3-t1

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function include_template($name, $data) {
+    $name = 'templates/' . $name;
+    $result = '';
+
+    if (!is_readable($name)) {
+        return $result;
+    }
+
+    ob_start();
+    extract($data);
+    require $name;
+
+    $result = ob_get_clean();
+
+    return $result;
+}
