48f2aa076da5bbbb136d51003491eb30bca99492
xlopok <44916594+xlopok@users.noreply.github.com>
2019-02-27 14:27:52 -0800

    добавлен параметр запроса в ссылку каждого лота на главной

--- templates/index.php
+++ templates/index.php
@@ -24,7 +24,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?=htmlspecialchars($item['category_name'])?></span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.php"><?=$item['lot_name']?>а</a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="lot.php?lot_id=<?= $item['id']; ?>"><?=$item['lot_name']?>а</a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
