3e18ba8c5654891c261f11c05f2cadd6ee9b3af1
xlopok <44916594+xlopok@users.noreply.github.com>
2019-02-28 23:15:44 -0800

    m6 t1 additions

--- lot.php
+++ lot.php
@@ -11,45 +11,21 @@ require_once('mysql_connect.php'); // Подключение к бд
 
 mysqli_set_charset($link, "utf8"); // установка кодировки к бд
 
-// Функции для показа лотов и категорий
 
-// Вызов функция для показа лотов 
-$lots_rows = get_lots($link);
 
 // Вызов функция для показа категорий 
-$categories_rows = get_catagories($link);
-// var_dump($categories_rows);
-?>
-
-<?php
 
 
-// if (!isset($_GET['lot_id'])) {
-//     // die('<b>Отсутствует id лота в запросе или такого параметра нет</b>');
-//     // header("HTTP/1.0 404 Not Found");
-//     $page_content = include_template('404.php', [
-//         'error' => 'Такого лота нет'
-//     ]);
+// Условие для параметра запроса и отображение нужногоа лота по его id
 
-// } 
-// else {
-//     $lot_id = $_GET['lot_id'];
-// }
-
-// $lot = get_lot($link, $lot_id);
-// // var_dump($lot);
-// if (isset($lot)) {
-//     $page_content = include_template('lot.php', 
-//     ['lots_rows' => $lots_rows, 
-//     'categories_rows' => $categories_rows,
-//     'lot' => $lot] );
-// }
 if (isset($_GET['lot_id']) && $_GET['lot_id'] !== '') {
 
      $lot_id = $_GET['lot_id']; 
      $lot = get_lot($link, $lot_id);
 
-     if(isset($lot)) {
+     if(!is_null(($lot))) {
+        $categories_rows = get_catagories($link);
+
         $page_content = include_template('lot.php', 
             ['lots_rows' => $lots_rows, 
             'categories_rows' => $categories_rows,
@@ -68,8 +44,6 @@ else {
 }
 
 
-
-// $page_content = include_template('lot.php', ['lots_rows' => $lots_rows, 'categories_rows' => $categories_rows] );
 $layout_content = include_template('layout.php', ['content' =>$page_content, 'title' => 'Страница лота', 'user_name' => $user_name, 'is_auth' => $is_auth, 'categories_rows' => $categories_rows]);
 
 print($layout_content);
